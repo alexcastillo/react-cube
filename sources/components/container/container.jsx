486f282e403ec14ce70d625779bc01af25c784cb
Vladimir Galushko <humanamburu@ojooo.com>
2016-05-28 20:58:07 +0300

    Add container component

---
+++ sources/components/container/container.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class Container extends Component {
+
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
--- sources/index.jsx
+++ sources/index.jsx
@@ -1,2 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Container from './components/container/container.jsx';
+
+render(<Container/>, document.getElementById('react-root'));
