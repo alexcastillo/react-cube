b6c29d6610af96d3720d741ab95e67dbfc3e1e55
humanamburu <humanamburu@ojooo.com>
2016-06-09 18:51:22 +0300

    Add lodash map method. Clean up translate getter.

--- sources/components/cube/cube.jsx
+++ sources/components/cube/cube.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
+import _ from 'lodash';
 
 import Face from '../face/face.jsx';
-import { getStylesForFaces } from '../../helpers/faces.jsx';
+import { getTranslateStylesForFaces } from '../../helpers/faces.jsx';
 import './cube.styl';
 
 export default class Cube extends Component {
 
     render() {
-        const faces = getStylesForFaces(this.props.size);
+        const faces = getTranslateStylesForFaces(this.props.size);
         const options = {
             width: `${this.props.size}px`,
             height: `${this.props.size}px`,
@@ -18,7 +19,7 @@ export default class Cube extends Component {
         return (
             <div className='cube' style={options}>
                 {
-                    faces.map((face, index) => {
+                    _.map(faces, (face, index) => {
                         return <Face {...face} key={index} {...options}/>
                     })
                 }
--- sources/helpers/faces.jsx
+++ sources/helpers/faces.jsx
@@ -1,27 +1,19 @@
 
-//TODO: implement isomorphic function with color mapping
-export function getStylesForFaces(size) {
+//TODO: implement isomorphic function
+export function getTranslateStylesForFaces(size) {
     const HALF_SIZE = size / 2;
-    const faces = [
-        {
-            transform: `translateZ(-${HALF_SIZE}px)`
-        },
-        {
-            transform: `translateX(-${HALF_SIZE}px) rotateY(90deg)`
-        },
-        {
-            transform: `translateX(${HALF_SIZE}px) rotateY(90deg)`
-        },
-        {
-            transform: `translateY(-${HALF_SIZE}px) rotateX(90deg)`
-        },
-        {
-            transform: `translateY(${HALF_SIZE}px) rotateX(90deg)`
-        },
-        {
-            transform: `translateZ(${HALF_SIZE}px)`
-        }
+    const translateStyles = [
+        `translateZ(-${HALF_SIZE}px)`,
+        `translateX(-${HALF_SIZE}px) rotateY(90deg)`,
+        `translateX( ${HALF_SIZE}px) rotateY(90deg)`,
+        `translateY(-${HALF_SIZE}px) rotateX(90deg)`,
+        `translateY( ${HALF_SIZE}px) rotateX(90deg)`,
+        `translateZ( ${HALF_SIZE}px)`
     ];
+    const facesStyles = [];
 
-    return faces;
+    return _.map(translateStyles, (item) => {
+        return { transform: item };
+    });
+
 }
