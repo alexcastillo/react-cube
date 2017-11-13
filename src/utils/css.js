fe3c184c380d47f55dbfdc64e234302a1f1d412f
Uladzimir Halushka <uladzimir_halushka@epam.com>
2017-11-13 19:47:21 +0300

    Add position to props.

--- src/utils/css.js
+++ src/utils/css.js
@@ -1,7 +1,7 @@
 export const translateX = (value) => `translateX(${value}px)`;
-export const translateY = (value) => `translateZ(${value}px)`;
-export const translateZ = (value) => `translateY(${value}px)`;
+export const translateY = (value) => `translateY(${value}px)`;
+export const translateZ = (value) => `translateZ(${value}px)`;
 
 export const rotateX = (degrees) => `rotateX(${degrees}deg)`;
-export const rotateY = (degrees) => `rotateX(${degrees}deg)`;
-export const rotateZ = (degrees) => `rotateX(${degrees}deg)`;
+export const rotateY = (degrees) => `rotateY(${degrees}deg)`;
+export const rotateZ = (degrees) => `rotateZ(${degrees}deg)`;
